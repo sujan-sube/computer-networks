7ae78fc197902af87f5c25982a6b79b06d50e1f4
Sujan Subendran <ssujan1@hotmail.com>
2016-10-03 13:36:56 -0400

    added calculation of output variables for M/D/1 queue

--- Project 1/simulator.py
+++ Project 1/simulator.py
@@ -1,11 +1,16 @@
 import numpy as np
 from asyncio import Queue
 
-# set global variables (tick duration set to 1ms)
+# set global variables (tick duration set to 1 ms)
 TICK_DURATION = 10**-3
-TICKS = 100
-SERVICE_TIME = None
+TICKS = 100000
+SERVICE_TIME = 0
 DASH = '---------------------------'
+AVG_NUM_PACKETS = 0
+AVG_SOJOURN_TICK = 0
+IDLE_TICK = 0
+TOTAL_PACKETS = 0
+
 
 # calculate exponential random variable
 def expon_var(lam):
@@ -15,8 +20,14 @@ def expon_var(lam):
 
 # implementation of M/D/1 queue
 def md1(action, q, i, service_tick):
+
+  # handle generate packet request
   if action == 'gen':
     q.put_nowait(i)
+    global TOTAL_PACKETS
+    TOTAL_PACKETS += 1
+
+  # handle service packet request
   elif action == 'service':
     if q.empty() is False:
       if i == 1 or service_tick < i:
@@ -24,9 +35,17 @@ def md1(action, q, i, service_tick):
 
       if i == service_tick:
         service_tick = i + SERVICE_TIME
-        q.get_nowait()
+        global AVG_SOJOURN_TICK
+        AVG_SOJOURN_TICK += i - q.get_nowait()
+        print("AVG_SOJOURN_TIME: ", AVG_SOJOURN_TICK)
 
+      global AVG_NUM_PACKETS
+      AVG_NUM_PACKETS += q.qsize()
       print("SERVICE TICK: ", service_tick)
+
+    elif q.empty() is True:
+      global IDLE_TICK
+      IDLE_TICK += 1
 
   return q, service_tick
 
@@ -52,7 +71,11 @@ def main(queue_type):
   C = 10**6
   L = 2000
   # K = None
-  global SERVICE_TIME 
+  global SERVICE_TIME
+  global IDLE_TICK
+  global AVG_NUM_PACKETS
+  global AVG_SOJOURN_TICK
+  global TOTAL_PACKETS
   SERVICE_TIME = int((float(L) / C) / TICK_DURATION)
 
   for rho in np.arange(lower, upper, step_size):
@@ -60,8 +83,14 @@ def main(queue_type):
     for repeat in range(0, repetition, 1):
       print("REPETITION: ", repeat)
       print(DASH)
+
+      # reset variables for each simulation
       inter_arrival_tick = 1
       service_tick = 1
+      IDLE_TICK = 0
+      AVG_NUM_PACKETS = 0
+      AVG_SOJOURN_TICK = 0
+      TOTAL_PACKETS = 0
 
       # network simulation loop
       for i in range(1, TICKS + 1, 1):
@@ -80,6 +109,21 @@ def main(queue_type):
         # print packets in queue
         print("PACKETS IN QUEUE: ", server_queue.qsize())
 
+      AVG_NUM_PACKETS /= TICKS
+      AVG_SOJOURN_TICK /= TOTAL_PACKETS
+
 
 # Network queue type and parameter selection
 main(queue_type='md1')
+
+print(DASH)
+print("E[n]: ", AVG_NUM_PACKETS)
+print("E[t]: ", AVG_SOJOURN_TICK)
+print("P_idle: ", IDLE_TICK)
+
+# Test Random Exponential Variable
+# mylist = []
+# for i in range(1, 1000):
+#   mylist.append(expon_var(0.3))
+
+# print(np.average(mylist))
